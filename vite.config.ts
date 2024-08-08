ef1bf89c5c93db0b3d0a85d20091f293025073e8
Jeb Feng <ranzeplay@outlook.com>
2024-08-08 23:34:48 +0800

    Separate chunks and extend chunk size limit
    To fit size of Excalidraw

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,20 @@ export default defineConfig({
   define: {
     'process.env': {}
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react': ['react'],
+          'react-dom': ['react-dom'],
+          'react-router-dom': ['react-router-dom'],
+          'excalidraw': ['@excalidraw/excalidraw'],
+          'headlessui': ['@headlessui/react'],
+          'tiptap': ['@tiptap/react'],
+          'lucide': ['lucide-react'],
+        }
+      }
+    },
+    chunkSizeWarningLimit: 2500
+  }
 })
